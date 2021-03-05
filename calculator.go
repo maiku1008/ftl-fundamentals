5582a94563b12af30e9b9fb7f1a959e80be8c7c2
Michael Cuffaro <michael@cuffaro.com>
2021-03-05 16:55:05 +0100

    feat: ftl-fundamentals main and bonus goals

--- calculator.go
+++ calculator.go
@@ -1,13 +1,125 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
-// Add takes two numbers and returns the result of adding them together.
-func Add(a, b float64) float64 {
-return a + b
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+// Add takes a list of numbers and returns the result of adding them together.
+func Add(nums ...float64) float64 {
+	switch len(nums) {
+	case 0:
+		return 0
+	case 1:
+		return nums[0]
+	default:
+		var sum float64
+		for _, n := range nums {
+			sum += n
+		}
+		return sum
+	}
 }
 
-// Subtract takes two numbers and returns the result of subtracting the second
-// from the first.
-func Subtract(a, b float64) float64 {
-	return b - a
+// Subtract takes a list of numbers and returns the result of subtracting them from each other.
+func Subtract(nums ...float64) float64 {
+	switch len(nums) {
+	case 0:
+		return 0
+	case 1:
+		return nums[0]
+	default:
+		var sum float64 = nums[0]
+		for _, n := range nums[1:] {
+			sum -= n
+		}
+		return sum
+	}
+}
+
+// Multiply takes a list of numbers and multiplies them.
+func Multiply(nums ...float64) float64 {
+	switch len(nums) {
+	case 0:
+		return 0
+	case 1:
+		return nums[0]
+	default:
+		var product float64 = nums[0]
+		for _, n := range nums[1:] {
+			product *= n
+		}
+		return product
+	}
+}
+
+// Divide takes a list of numbers and divides them.
+// Error is returned when dividing by 0.
+func Divide(nums ...float64) (float64, error) {
+	switch len(nums) {
+	case 0:
+		return 0, nil
+	case 1:
+		return nums[0], nil
+	default:
+		var product float64 = nums[0]
+		for _, n := range nums[1:] {
+			if n == 0 || product == 0 {
+				return 0, fmt.Errorf("cannot divide by zero")
+			}
+			product /= n
+		}
+		return product, nil
+	}
+}
+
+// Sqrt finds the square root of a number
+// Running this on 0 or negative numbers returns an error
+func Sqrt(num float64) (float64, error) {
+	if num < 0 {
+		return 0, fmt.Errorf("not allowed to find the square root of a negative numbers")
+	}
+	return math.Sqrt(num), nil
+}
+
+// StringMath takes a string, parses it and runs the math operation found therein
+// Returns an error if it cannot do so
+func StringMath(input string) (float64, error) {
+	var a, b float64
+	var operation string
+	var err error
+
+	operations := []string{"+", "-", "*", "/"}
+	input = strings.ReplaceAll(input, " ", "")
+	for _, op := range operations {
+		if strings.Contains(input, op) {
+			numbers := strings.Split(input, op)
+			operation = op
+			a, err = strconv.ParseFloat(numbers[0], 64)
+			if err != nil {
+				return 0, err
+			}
+			b, err = strconv.ParseFloat(numbers[1], 64)
+			if err != nil {
+				return 0, err
+			}
+			break
+		}
+	}
+
+	switch operation {
+	case "+":
+		return Add(a, b), nil
+	case "-":
+		return Subtract(a, b), nil
+	case "*":
+		return Multiply(a, b), nil
+	case "/":
+		return Divide(a, b)
+	default:
+		return 0, fmt.Errorf("no operator found in string")
+	}
 }
--- calculator_test.go
+++ calculator_test.go
@@ -2,23 +2,189 @@ package calculator_test
 
 import (
 	"calculator"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	var testCases = []struct {
+		name string
+		nums []float64
+		want float64
+	}{
+		{"positive", []float64{2, 2}, 4},
+		{"negative", []float64{-2, 5}, 3},
+		{"one zero", []float64{0, 3}, 3},
+		{"two zeroes", []float64{0, 0}, 0},
+		{"decimal", []float64{2.5, 4}, 6.5},
+		{"empty slice", []float64{}, 0},
+		{"one item in slice", []float64{3}, 3},
+		{"many numbers", []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 55},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.nums...)
+		if tc.want != got {
+			t.Errorf("test name: %s, want %f, got %f", tc.name, tc.want, got)
+		}
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+func TestAddRandom(t *testing.T) {
+	t.Parallel()
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < 1000; i++ {
+		a, b := rand.Float64(), rand.Float64()
+		want := a + b
+		got := calculator.Add(a, b)
+		if want != got {
+			t.Errorf("adding %f, %f: want %f, got %f", a, b, want, got)
+		}
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var testCases = []struct {
+		name string
+		nums []float64
+		want float64
+	}{
+		{"positive", []float64{2, 2}, 0},
+		{"negative", []float64{-2, 5}, -7},
+		{"one zero", []float64{0, 3}, -3},
+		{"two zeroes", []float64{0, 0}, 0},
+		{"decimal", []float64{2.5, 4}, -1.5},
+		{"empty slice", []float64{}, 0},
+		{"one item in slice", []float64{3}, 3},
+		{"many numbers", []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, -53},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.nums...)
+		if tc.want != got {
+			t.Errorf("test name: %s, want %f, got %f", tc.name, tc.want, got)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var testCases = []struct {
+		name string
+		nums []float64
+		want float64
+	}{
+		{"positive", []float64{2, 2}, 4},
+		{"negative", []float64{-2, 5}, -10},
+		{"one zero", []float64{0, 3}, 0},
+		{"two zeroes", []float64{0, 0}, 0},
+		{"decimal", []float64{2.5, 4}, 10},
+		{"empty slice", []float64{}, 0},
+		{"one item in slice", []float64{3}, 3},
+		{"many numbers", []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3628800},
+		{"many of the same numbers", []float64{2, 2, 2, 2, 2, 2, 2, 2, 2, 2}, 1024},
+	}
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.nums...)
+		if tc.want != got {
+			t.Errorf("test name: %s, want %f, got %f", tc.name, tc.want, got)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	var testCases = []struct {
+		name        string
+		nums        []float64
+		want        float64
+		errExpected bool
+	}{
+		{"positive", []float64{2, 2}, 1, false},
+		{"negative", []float64{-2, 5}, -0.4, false},
+		{"one zero", []float64{0, 3}, 0, true},
+		{"two zeroes", []float64{0, 0}, 0, true},
+		{"decimal", []float64{2.5, 4}, 0.625, false},
+		{"empty slice", []float64{}, 0, false},
+		{"one item in slice", []float64{3}, 3, false},
+		{"many numbers", []float64{1024, 2, 2, 2, 2, 2, 2, 2, 2, 2}, 2, false},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.nums...)
+		if tc.errExpected {
+			if err == nil {
+				t.Errorf("test name: %s expected error, got nil", tc.name)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("test name: %s returned unexpected error: %s", tc.name, err)
+			}
+			if tc.want != got {
+				t.Errorf("test name: %s, want %f, got %f", tc.name, tc.want, got)
+			}
+		}
+	}
+}
+
+func TestSqrt(t *testing.T) {
+	t.Parallel()
+	var testCases = []struct {
+		name        string
+		num, want   float64
+		errExpected bool
+	}{
+		{"positive", 4, 2, false},
+		{"negative", -25, 5, true},
+		{"zero", 0, 0, false},
+		{"decimal", 30.25, 5.5, false},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Sqrt(tc.num)
+		if tc.errExpected {
+			if err == nil {
+				t.Errorf("test name: %s expected error, got nil", tc.name)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("test name: %s returned unexpected error: %s", tc.name, err)
+			}
+			if tc.want != got {
+				t.Errorf("test name: %s, want %f, got %f", tc.name, tc.want, got)
+			}
+		}
+	}
+}
+
+func TestStringMath(t *testing.T) {
+	t.Parallel()
+	var testCases = []struct {
+		name        string
+		input       string
+		want        float64
+		errExpected bool
+	}{
+		{"multiply", "2*2", 4, false},
+		{"add", "1 + 1.5", 2.5, false},
+		{"divide", "18   /   6", 3, false},
+		{"subtract", "100-0.1", 99.9, false},
+		{"bogus input", "foo_bar_baz", 0, true},
+		{"return an error passed from other function", "1/0", 0, true},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.StringMath(tc.input)
+		if tc.errExpected {
+			if err == nil {
+				t.Errorf("test name: %s expected error, got nil", tc.name)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("test name: %s returned unexpected error: %s", tc.name, err)
+			}
+			if tc.want != got {
+				t.Errorf("test name: %s, want %f, got %f", tc.name, tc.want, got)
+			}
+		}
+	}
+}
